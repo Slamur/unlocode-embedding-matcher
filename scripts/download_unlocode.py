69a3d70696bf39193299e06c6c06e7ec3d52f998
Slava Muravev <SlamurMunch@gmail.com>
2026-03-08 18:31:14 +0600

    Download unlocode: added raw subfolder

--- scripts/download_unlocode.py
+++ scripts/download_unlocode.py
@@ -6,40 +6,48 @@ from pathlib import Path
 URL = "https://service.unece.org/trade/locode/loc242csv.zip"
 
 DATA_DIR = Path("data")
-ZIP_PATH = DATA_DIR / "unlocode.zip"
+RAW_DIR = DATA_DIR / "raw"
+
+ZIP_FILENAME = "unlocode.zip"
 
 STREAM_CHUNK_SIZE = 8192
 
 
-def _download_stream(response: requests.Response, destination: Path = ZIP_PATH, chunk_size: int = STREAM_CHUNK_SIZE):
+def _download_stream(response: requests.Response, destination: Path, chunk_size: int = STREAM_CHUNK_SIZE):
     with open(destination, "wb") as f:
         for chunk in response.iter_content(chunk_size=chunk_size):
             f.write(chunk)
 
 
-def _download_zip(url: str = URL, destination: Path = ZIP_PATH):
-    print("Downloading UN/LOCODE dataset from", url)
+def _download_zip(url: str = URL, destination_dir: Path = RAW_DIR) -> Path:
+    print(f"Downloading UN/LOCODE dataset from {url} to {destination_dir}")
 
-    destination.parent.mkdir(parents=True, exist_ok=True)
+    destination_dir.mkdir(parents=True, exist_ok=True)
+
+    destination = destination_dir / ZIP_FILENAME
 
     with requests.get(url, stream=True) as response:
         response.raise_for_status()
 
         _download_stream(response, destination, chunk_size=STREAM_CHUNK_SIZE)
 
+    return destination
 
-def _unzip(src: Path = ZIP_PATH, dest: Path = DATA_DIR):
-    print("Extracting dataset from", src)
-    
-    with zipfile.ZipFile(src, "r") as z:
+
+def _unzip(source: Path, dest: Path = RAW_DIR):
+    print(f"Extracting dataset from {source} to {dest}")
+
+    with zipfile.ZipFile(source, "r") as z:
         z.extractall(dest)
 
 
 def main():
-    _download_zip()
-    _unzip()
+    destination_dir = RAW_DIR
 
-    print("Dataset ready in", DATA_DIR)
+    zip_filename = _download_zip(destination_dir=destination_dir)
+    _unzip(source=zip_filename, dest=destination_dir)
+
+    print("Dataset ready in", destination_dir)
 
 
 if __name__ == "__main__":
