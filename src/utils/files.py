3139ed35f3a57727e9ea0057e30871b6fc5541cf
Slava Muravev <SlamurMunch@gmail.com>
2026-03-11 23:50:28 +0600

    Files: read_parquet

--- src/utils/files.py
+++ src/utils/files.py
@@ -16,3 +16,9 @@ def ensure_parent_dir_exists(path: Path) -> Path:
 def save_parquet(df: pd.DataFrame, path: Path) -> None:
     ensure_parent_dir_exists(path=path)
     df.to_parquet(path, index=False)
+
+
+def read_parquet(path: Path) -> pd.DataFrame:
+    if not path.exists():
+        raise FileNotFoundError(f"File not found at: {path}")
+    return pd.read_parquet(path)
