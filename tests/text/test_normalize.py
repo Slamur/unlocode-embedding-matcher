d65092f3e2db7779a40b985f94530faf7eeb5d25
Slava Muravev <SlamurMunch@gmail.com>
2026-03-14 20:59:02 +0600

    Text: test for normalization moved

--- tests/text/test_normalize.py
+++ tests/text/test_normalize.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.dataset.preparation.search_texts import _normalize_search_text
+from src.text.normalize import normalize_text
 
 
 @pytest.mark.parametrize(
@@ -27,17 +27,17 @@ from src.dataset.preparation.search_texts import _normalize_search_text
     ],
 )
 def test_normalize_search_text(raw_text: str, expected: str) -> None:
-    assert _normalize_search_text(raw_text) == expected
+    assert normalize_text(raw_text) == expected
 
 
 def test_normalize_search_text_collapses_multiple_separators_to_single_space() -> None:
-    assert _normalize_search_text("foo, - / bar") == "foo bar"
+    assert normalize_text("foo, - / bar") == "foo bar"
 
 
 def test_normalize_search_text_is_idempotent() -> None:
     raw_text = "  Brussel (Bruxelles), BE  "
 
-    normalized_once = _normalize_search_text(raw_text)
-    normalized_twice = _normalize_search_text(normalized_once)
+    normalized_once = normalize_text(raw_text)
+    normalized_twice = normalize_text(normalized_once)
 
     assert normalized_twice == normalized_once
