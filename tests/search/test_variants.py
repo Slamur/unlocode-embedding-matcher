beb9a2d970b537e32e7b09064ec98272374f15aa
Slava Muravev <SlamurMunch@gmail.com>
2026-03-19 00:29:25 +0600

    Search: updated variant tests

--- tests/search/test_variants.py
+++ tests/search/test_variants.py
@@ -6,16 +6,21 @@ def test_build_query_variants_returns_empty_list_for_empty_input() -> None:
 
 
 def test_build_query_variants_returns_full_query_variant() -> None:
-    assert build_query_variants("bishkek kyrgyzstan") == [
+    assert build_query_variants("bishkek kyrgyzstan manas") == [
         QueryVariant(
-            text="bishkek kyrgyzstan",
+            text="bishkek kyrgyzstan manas",
             kind="full_query",
             weight=1.00,
         ),
         QueryVariant(
             text="location bishkek",
-            kind="reverse_city_second",
-            weight=1.07,
+            kind="forward_city_first",
+            weight=1.03,
+        ),
+        QueryVariant(
+            text="location manas",
+            kind="reverse_city_first",
+            weight=1.03,
         ),
         QueryVariant(
             text="location kyrgyzstan",
@@ -29,23 +34,38 @@ def test_build_query_variants_returns_full_query_variant() -> None:
         ),
         QueryVariant(
             text="location bishkek country kyrgyzstan",
+            kind="forward_city_first_country_second",
+            weight=1.10,
+        ),
+        QueryVariant(
+            text="location kyrgyzstan country manas",
             kind="reverse_city_second_country_first",
+            weight=1.10,
+        ),
+        QueryVariant(
+            text="location manas country kyrgyzstan",
+            kind="reverse_city_first_country_second",
             weight=1.10,
         ),
     ]
 
 
 def test_build_query_variants_removes_tokens_with_digits() -> None:
-    assert build_query_variants("samara, novo sadovaya, 1/2") == [
+    assert build_query_variants("samara, novo sadovaya, 1/2, naba") == [
         QueryVariant(
-            text="samara, novo sadovaya, 1/2",
+            text="samara, novo sadovaya, 1/2, naba",
             kind="full_query",
             weight=1.00,
         ),
         QueryVariant(
             text="location samara",
-            kind="reverse_city_second",
-            weight=1.07,
+            kind="forward_city_first",
+            weight=1.03,
+        ),
+        QueryVariant(
+            text="location naba",
+            kind="reverse_city_first",
+            weight=1.03,
         ),
         QueryVariant(
             text="location novo sadovaya",
@@ -59,7 +79,17 @@ def test_build_query_variants_removes_tokens_with_digits() -> None:
         ),
         QueryVariant(
             text="location samara country novo sadovaya",
+            kind="forward_city_first_country_second",
+            weight=1.10,
+        ),
+        QueryVariant(
+            text="location novo sadovaya country naba",
             kind="reverse_city_second_country_first",
+            weight=1.10,
+        ),
+        QueryVariant(
+            text="location naba country novo sadovaya",
+            kind="reverse_city_first_country_second",
             weight=1.10,
         ),
     ]
